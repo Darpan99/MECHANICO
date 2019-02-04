fd967f1888312c16bedbcb9f3316cae79b2bdb94
Darpan99 <darpanbendre@gmail.com>
2019-02-04 15:02:38 +0530

    hum app banayenge

---
+++ app/src/main/java/com/example/darpan/mechanico/PAGE3.java
@@ -0,0 +1,33 @@
+package com.example.darpan.mechanico;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class PAGE3  extends AppCompatActivity {
+    Button btnLogOut;
+    FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page3);
+        btnLogOut = (Button) findViewById(R.id.btnLogOut);
+        btnLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                FirebaseAuth.getInstance().signOut();
+                Intent I = new Intent(PAGE3.this, REGISTRATION.class);
+                startActivity(I);
+
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/darpan/mechanico/Splash.java
@@ -0,0 +1,40 @@
+package com.example.darpan.mechanico;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Splash extends AppCompatActivity {
+private TextView tv;
+private ImageView iv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+        tv= (TextView)findViewById(R.id.tv);
+        iv= (ImageView)findViewById(R.id.iv);
+        Animation myanim= AnimationUtils.loadAnimation(this,R.anim.mytransistion);
+        tv.startAnimation(myanim);
+        iv.startAnimation(myanim);
+        final Intent i= new Intent(this,REGISTRATION.class);
+        Thread timer= new Thread(){
+            public void run (){
+                try{
+                    sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                   startActivity(i);
+                   finish();
+                }
+            }
+        };
+            timer.start();
+    }
+}
