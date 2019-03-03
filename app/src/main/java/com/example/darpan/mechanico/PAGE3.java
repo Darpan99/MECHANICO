4f308b36ac969f1197bbde875e6d631cf270d84b
Vidhi Agrawal <vidhi.agrawal03@gmail.com>
2019-03-04 00:37:13 +0530

    Committed by Vidhi!!!!!

--- app/src/main/java/com/example/darpan/mechanico/PAGE3.java
+++ app/src/main/java/com/example/darpan/mechanico/PAGE3.java
@@ -4,25 +4,57 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class PAGE3  extends AppCompatActivity {
     Button btnLogOut;
     FirebaseAuth firebaseAuth;
-    private FirebaseAuth.AuthStateListener authStateListener;
+    DrawerLayout dl;
+    ActionBarDrawerToggle abdt;
+
+     private FirebaseAuth.AuthStateListener authStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page3);
         getSupportActionBar().hide();
-        btnLogOut = (Button) findViewById(R.id.btnlogout);
+        btnLogOut = findViewById(R.id.btnlogout)
+        dl= findViewById(R.id.DL);
+        abdt=new ActionBarDrawerToggle(this,dl,R.string.Open,R.string.Close);
+        abdt.setDrawerIndicatorEnabled(true);
+        dl.addDrawerListener(abdt);
+        abdt.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        NavigationView n_view= findViewById(R.id.nav_view);
+        n_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                int id = menuItem.getItemId();
+
+                if(id==R.id.item_1)
+                {
+                    Toast.makeText(PAGE3.this,"My Profile",Toast.LENGTH_LONG).show();
+                }
+
+                return true;
+            }
+        });
+
         btnLogOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -54,5 +86,10 @@ public class PAGE3  extends AppCompatActivity {
             return;
         }
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return abdt.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
     }
 }
