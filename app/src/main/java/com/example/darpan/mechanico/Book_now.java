3acc56fd38336dd10e329c8205a7208c5084e155
Vidhi Agrawal <vidhi.agrawal03@gmail.com>
2019-03-09 16:32:19 +0530

    vidhi committed

--- app/src/main/java/com/example/darpan/mechanico/Book_now.java
+++ app/src/main/java/com/example/darpan/mechanico/Book_now.java
@@ -16,9 +16,9 @@ import java.util.ArrayList;
 
 public class Book_now extends AppCompatActivity {
 
-    Button car;
-    TextView itemselected;
-    String[] listItems;
+    Button car,model,type;
+    TextView itemselected,itemselected1,itemselected2;
+    String[] listItems,listhonda;
     boolean[] checkedItems;
     ArrayList<Integer> mUserItems = new ArrayList<>();
 
@@ -36,8 +36,14 @@ public class Book_now extends AppCompatActivity {
         spinner1.setAdapter(myadapter);
 
         car=(Button)findViewById(R.id.carbtn);
+        model=(Button)findViewById(R.id.modelbtn);
+        type=(Button)findViewById(R.id.typebtn);
         itemselected=(TextView)findViewById(R.id.cartext);
-        listItems = getResources().getStringArray(R.array.service);
+        itemselected1=(TextView)findViewById(R.id.modeltext);
+        itemselected=(TextView)findViewById(R.id.typetext);
+        listItems = getResources().getStringArray(R.array.cars);
+        listhonda = getResources().getStringArray(R.array.Honda);
+
         checkedItems = new boolean[listItems.length];
 
         car.setOnClickListener(new View.OnClickListener() {
@@ -58,6 +64,31 @@ public class Book_now extends AppCompatActivity {
                 mDialog.show();
             }
         });
+
+        if(itemselected.equals("Honda"))
+        {
+            model.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    final AlertDialog.Builder mBuilder = new AlertDialog.Builder(Book_now.this);
+                    mBuilder.setTitle("Select the car");
+                    mBuilder.setSingleChoiceItems(listhonda, -1, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int i) {
+                            itemselected.setText(listhonda[i]);
+                            dialog.dismiss();
+
+
+                        }
+                    });
+                    AlertDialog mDialog = mBuilder.create();
+                    mDialog.show();
+
+                }
+            });
+        }
+
 
 
     }
