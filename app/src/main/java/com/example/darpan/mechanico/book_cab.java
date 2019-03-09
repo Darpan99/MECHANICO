8f5a702cb4d4e62bf4b128c0313e7f36313d210a
Sadhikabhatia <sadhikabhatia781@gmail.com>
2019-03-09 19:02:41 +0530

    committed by Sadhika

--- app/src/main/java/com/example/darpan/mechanico/book_cab.java
+++ app/src/main/java/com/example/darpan/mechanico/book_cab.java
@@ -1,7 +1,9 @@
 package com.example.darpan.mechanico;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class book_cab extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class book_cab extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_cab);
+    }
+
+    public void bookuber(View view) {
+        Intent intent = getPackageManager().getLaunchIntentForPackage("com.ubercab");
+        startActivity(intent);
+    }
+
+    public void bookola(View view) {
+        Intent intent = getPackageManager().getLaunchIntentForPackage("com.olacabs");
+        startActivity(intent);
     }
 }
